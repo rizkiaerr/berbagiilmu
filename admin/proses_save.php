22b51961158f69164a8f2a1159628f3d405cbb2e
unknown <rizkiaerr@gmail.com>
2017-06-04 14:05:19 +0700

    sudah diperbaiki

--- admin/proses_save.php
+++ admin/proses_save.php
@@ -8,12 +8,12 @@ include "config.php";
 	$member_tglahir	 = $_POST['member_tglahir'];
 	$member_alamat 	 = $_POST['member_alamat'];
 	$member_username = $_POST['member_username'];
+	$member_tlp 	 = $_POST['member_tlp'];
 	$member_email 	 = $_POST['member_email'];
 	$member_password = md5($_POST['member_password']);
 
-
-
 mysqli_query($link,"INSERT INTO member(member_id, member_nama, member_jk, member_ttl, member_tglahir, member_alamat, member_username, member_tlp, member_email, member_password) VALUES
-	('$member_id','$member_nama','$member_jk','$member_ttl','$member_tglahir','$member_alamat','$member_username','$member_email','$member_password')");
-header('location:member.php');
+		     ('$member_id','$member_nama','$member_jk','$member_ttl','$member_tglahir','$member_alamat','$member_username','$member_tlp','$member_email','$member_password')");
+
+header('location:member.php'); 
 ?>
