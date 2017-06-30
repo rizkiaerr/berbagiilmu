494ebb302cccd8c0e5a3e0a45f3e3ce459df0760
Kang Angga <angga3399@gmail.com>
2017-06-30 23:33:40 +0700

    Update modal_edit.php

--- admin/modal_edit.php
+++ admin/modal_edit.php
@@ -1,5 +1,6 @@
 <?php
-    require "config.php";
+  require "config.php";
+  if(!empty($_GET['member_id'])){
 	$member_id=$_GET['member_id'];
 	$modal=mysqli_query($link,"SELECT * FROM member WHERE member_id='$member_id'");
 	while($r=mysqli_fetch_array($modal)){
@@ -10,7 +11,7 @@
 
     	<div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-            <h4 class="modal-title" id="myModalLabel">Edit Data Menggunakan Modal Boostrap (popup)</h4>
+            <h4 class="modal-title" id="myModalLabel">Lihat Data Member <?php echo $r['member_nama']; ?></h4>
         </div>
 
         <div class="modal-body">
@@ -19,7 +20,8 @@
                 <div class="form-group" style="padding-bottom: 1px;">
                   <label for="Nama Member">Nama Member</label>
                     <input type="hidden" name="member_id"  class="form-control" value="<?php echo $r['member_id']; ?>" />
-                    <input type="text" name="member_nama"  class="form-control" value="<?php echo $r['member_nama']; ?>"/>
+                    <br>
+                    <?php echo $r['member_nama']; ?>
                 </div>
 
                 <div class="form-group" style="padding-bottom: 1px;">
@@ -27,56 +29,56 @@
                     <br>
                   <?php
                     if (($r['member_jk'])=='L') {
-                      echo "<input type='radio' name='member_jk' value='L' checked/>Laki-Laki";
-                      echo "<input type='radio' name='member_jk' value='P'/> Perempuan";
+                     $jk="Laki-Laki";
                     }else
                     {
-                      echo "<input type='radio' name='member_jk' value='L'/> Laki-Laki";
-                      echo "<input type='radio' name='member_jk' value='P' checked /> Perempuan";
+                      $jk="Perempuan";
                     }
+                    echo $jk;
                  ?>
                 </div>
 
                 <div class="form-group" style="padding-bottom: 1px;">
                   <label for="Tempat Lahir">Tempat Lahir</label>
-                    <input type="text" name="member_ttl"  class="form-control" value="<?php echo $r['member_ttl']; ?>"/>
+                    <br>
+                    <?php echo $r['member_ttl']; ?>
                 </div>
-               <div class="form-group" style="padding-bottom: 1px;">
+               
+                <div class="form-group" style="padding-bottom: 1px;">
                   <label for="Alamat">Tanggal Lahir</label>
-                    <input type="date" name="member_tglahir" class="form-control" value="<?php echo $r['member_tglahir']; ?>"/>
+                  <br>
+                  <?php echo date('d F Y',strtotime($r['member_tglahir'])); ?>
                 </div>
+                
                 <div class="form-group" style="padding-bottom: 1px;">
                   <label for="Alamat">Alamat</label>
-                    <textarea name="member_alamat"  class="form-control"><?php echo $r['member_alamat']; ?></textarea>
+                  <br>
+                  <?php echo $r['member_alamat']; ?>
                 </div>
+                
                 <div class="form-group" style="padding-bottom: 1px;">
                   <label for="No Telephone/Handphone">No Telephone/Handphone</label>
-                   <input type="text" name="member_tlp"  class="form-control" value="<?php echo $r['member_tlp']; ?>"/>
+                  <br>
+                  <?php echo $r['member_tlp']; ?>
                 </div>
                 <div class="form-group" style="padding-bottom: 1px;">
                   <label for="Username">Username</label>
-                  <input type="text" name="member_username"  class="form-control" value="<?php echo $r['member_username']; ?>"/>
+                  <br>
+                  <?php echo $r['member_username']; ?>
                 </div>
                 <div class="form-group" style="padding-bottom: 1px;">
                   <label for="Email">Email</label>
-                   <input type="text" name="member_email"  class="form-control" value="<?php echo $r['member_email']; ?>"/>
+                  <br>
+                  <?php echo $r['member_email']; ?>
                 </div>
 
                 <div class="form-group" style="padding-bottom: 1px;">
                   <label for="Password">Password</label>
-                   <input type="password" name="member_password"  class="form-control" value="<?php echo $r['member_password']; ?>"/>
+                  <br>
+                  <?php echo "*************"; ?>
                 </div>
 
-	            <div class="modal-footer">
-	                <button class="btn btn-success" type="submit">
-	                    Confirm
-	                </button>
-
-	                <button type="reset" class="btn btn-danger"  data-dismiss="modal" aria-hidden="true">
-	               		Cancel
-	                </button>
-	            </div>
-
+	            
             	</form>
 
              <?php } ?>
@@ -86,3 +88,128 @@
            
         </div>
     </div>
+<?php 
+}elseif(!empty($_GET['buku_id'])){
+  
+
+  $buku_id=$_GET['buku_id'];
+  $admin=mysqli_query($link,"SELECT buku_admin.*,admin.admin_nama,kategori.kategori_jenis FROM buku_admin,admin,kategori WHERE buku_admin.buku_author=admin.admin_id AND buku_admin.buku_kategori=kategori.kategori_id AND buku_admin.buku_id='$buku_id'");
+  $member=mysqli_query($link,"SELECT buku.*,member.member_nama,kategori.kategori_jenis FROM buku,member,kategori WHERE buku.buku_author=member.member_id AND buku.buku_kategori=kategori.kategori_id AND buku.buku_id='$buku_id'");
+  if(mysqli_num_rows($admin)==1){
+  while($r=mysqli_fetch_array($admin)){
+?>
+
+<div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+            <h4 class="modal-title" id="myModalLabel">Lihat Data Buku <?php echo $r['buku_judul']; ?></h4>
+        </div>
+
+        <div class="modal-body">
+          <form action="proses_save.php?save=buku_admin" name="modal_popup" enctype="multipart/form-data" method="POST">
+            
+                <input type="hidden" name="buku_id"  class="form-control" value="<?php echo $r['buku_id']; ?>" />
+                <div class="form-group" style="padding-bottom: 20px;">
+                  <label>Judul Buku</label>
+                    <input type="text" name="buku_judul"  class="form-control" value="<?php echo $r['buku_judul']; ?>"/>
+                </div>
+
+                <div class="form-group" style="padding-bottom: 20px;">
+                  <label>Penulis Buku</label>
+                    <input type="text" name="buku_penulis"  class="form-control" value="<?php echo $r['buku_penulis']; ?>"/>
+                </div>
+
+                <div class="form-group" style="padding-bottom: 20px;">
+                  <label>Bahasa</label>
+                   <input type="text" name="buku_bahasa"  class="form-control" value="<?php echo $r['buku_bahasa']; ?>"/>
+                </div>
+
+                <div class="form-group" style="padding-bottom: 20px;">
+                  <label for="Tanggal Lahir">Author</label>
+                    <input type="text" name="buku_author"  class="form-control" value="<?php echo $r['admin_nama']; ?>" readonly/>
+                </div>
+
+                <div class="form-group" style="padding-bottom: 20px;">
+                  <label>Kategori</label>
+                    <input type="text" name="buku_kategori"  class="form-control" value="<?php echo $r['kategori_jenis']; ?>" readonly/>
+                </div>
+
+                <div class="form-group" style="padding-bottom: 20px;">
+                  <label>Tgl Upload</label>
+                  <br>
+                  <input type="date" name="tgl_upload" value="<?php echo $r['tanggal_upload']; ?>"/ readonly>
+                </div>
+
+              <div class="modal-footer">
+                  <button class="btn btn-success" type="submit">
+                      Confirm
+                  </button>
+
+                  <button type="reset" class="btn btn-danger"  data-dismiss="modal" aria-hidden="true">
+                    Cancel
+                  </button>
+              </div>
+
+              </form>
+
+             <?php }//end while ?>
+            </div>
+        </div>
+    </div>
+    <?php 
+      }elseif(mysqli_num_rows($member)==1){
+        while($r=mysqli_fetch_array($member)){
+?>
+
+<div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+            <h4 class="modal-title" id="myModalLabel">Lihat Data Buku <?php echo $r['buku_judul']; ?></h4>
+        </div>
+
+        <div class="modal-body">
+          <form action="proses_save.php?save=buku_member" name="modal_popup" enctype="multipart/form-data" method="POST">
+            
+                <input type="hidden" name="buku_id"  class="form-control" value="<?php echo $r['buku_id']; ?>" />
+                <div class="form-group" style="padding-bottom: 20px;">
+                  <label>Judul Buku</label>
+                    <input type="text" name="buku_judul"  class="form-control" value="<?php echo $r['buku_judul']; ?>"/>
+                </div>
+
+                <div class="form-group" style="padding-bottom: 20px;">
+                  <label for="Tanggal Lahir">Author</label>
+                    <input type="text" name="buku_author"  class="form-control" value="<?php echo $r['member_nama']; ?>" readonly/>
+                </div>
+
+                <div class="form-group" style="padding-bottom: 20px;">
+                  <label>Tgl Upload</label>
+                  <br>
+                  <input type="date" name="tgl_upload" value="<?php echo $r['tanggal_upload']; ?>"/ readonly>
+                </div>
+
+              <div class="modal-footer">
+                  <button class="btn btn-success" type="submit">
+                      Confirm
+                  </button>
+
+                  <button type="reset" class="btn btn-danger"  data-dismiss="modal" aria-hidden="true">
+                    Cancel
+                  </button>
+              </div>
+
+              </form>
+
+             <?php }//end while fetch buku member ?>
+            </div>
+        </div>
+    </div>
+    <?php 
+      } //end if mysqli row member
+    ?>
+<?php 
+}
+?>
