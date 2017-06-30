6b79696956e2b0014e55f9c4ce6ef0594e8b2801
Kang Angga <angga3399@gmail.com>
2017-06-30 23:37:12 +0700

    angga

--- admin/proses_delete.php
+++ admin/proses_delete.php
@@ -1,11 +1,19 @@
-<!--
-Author : Aguzrybudy
-Created : Selasa, 19-April-2016
-Title : Crud Menggunakan Modal Bootsrap
--->
 <?php
-	include "config.php";
+  include "config.php";
+
+  if(!empty($_GET['member_id'])){
 	$member_id=$_GET['member_id'];
 	$modal=mysqli_query($link,"DELETE FROM member WHERE member_id='$member_id'");
 	header('location:member.php');
+  
+  }elseif(!empty($_GET['buku_admin'])){
+	$buku_id=$_GET['buku_admin'];
+	$modal=mysqli_query($link,"DELETE FROM buku_admin WHERE buku_id='$buku_id'");
+	header('location:buku.php');
+  
+  }elseif(!empty($_GET['buku_member'])){
+	$buku_id=$_GET['buku_member'];
+	$modal=mysqli_query($link,"DELETE FROM buku WHERE buku_id='$buku_id'");
+	header('location:buku.php');
+  }
 ?>
